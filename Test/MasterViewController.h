93af08c41c43c99ddc96f108e59681cb281d419b
zhangyapeng <1365160860@qq.com>
2017-11-22 19:09:28 +0800

    www

---
+++ Test/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Test
+//
+//  Created by zhangyapeng on 16/6/23.
+//  Copyright © 2016年 zhangyapeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
