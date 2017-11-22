85deac2799b608bc32cf4699229770e340d58fd0
zhangyapeng01 <30308971+zhangyapeng01@users.noreply.github.com>
2017-11-22 17:10:06 +0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic, assign)UIInterfaceOrientation makeOrientation;
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
